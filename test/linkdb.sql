717d40356e8b9b275285bb3c3eefca0560b68adc
Noah Peart <noah.v.peart@gmail.com>
2024-08-03 15:36:40 -0700

    update packages; add faker-js

--- test/linkdb.sql
+++ test/linkdb.sql
@@ -45,7 +45,6 @@ CREATE TABLE group_tags (
   PRIMARY KEY (tag_id, group_id)
 );
 
----------------------------------------------------------------------
 --- Insert
 
 INSERT INTO links(title, url, type, description) 
@@ -54,7 +53,8 @@ VALUES ('a', 'https://a', 'href', 'aaaaa'),
        ('f', '~/f.txt', 'file', null);
 
 INSERT INTO tags(name, alias) 
-VALUES ('x-mode', 'x'), ('y-mode', 'y');
+VALUES ('x-mode', 'x'),
+       ('y-mode', 'y');
 
 INSERT INTO link_tags(link_id, tag_id) 
 VALUES (1, 1), (1, 2), (2, 2), (3, 1);
@@ -64,91 +64,91 @@ VALUES ('xy', 'both x and y');
 
 INSERT INTO group_tags(group_id, tag_id)
 SELECT group_id, tag_id
-  FROM tags t, (SELECT group_id FROM groups g WHERE g.name = 'xy')
- WHERE t.name IN ('x', 'y') OR t.alias IN ('x', 'y');
+FROM tags t, (SELECT group_id FROM groups g WHERE g.name = 'xy')
+WHERE t.name IN ('x', 'y') OR t.alias IN ('x', 'y');
 
 --- Update
 
 UPDATE tags 
-   SET alias = 'xx', name = 'xx-mode'
- WHERE name = 'x-mode';
+SET alias = 'xx', name = 'xx-mode'
+WHERE name = 'x-mode';
 
 UPDATE links
-   SET hits = hits + 1, accessed_at = CURRENT_TIMESTAMP, type = 'href'
- WHERE title = 'a';
+SET hits = hits + 1, accessed_at = CURRENT_TIMESTAMP, type = 'href'
+WHERE title = 'a';
 
 ---------------------------------------------------------------------
 --- Queries 
 
 -- Tags for link
 SELECT t.tag_id, IFNULL(t.alias, t.name) AS 'tag'
-  FROM links l
-       JOIN link_tags USING(link_id) 
-       JOIN tags t USING(tag_id)
+ FROM links l
+   JOIN link_tags USING(link_id) 
+   JOIN tags t USING(tag_id)
  WHERE l.title = 'a';
 
 SELECT t.tag_id, IFNULL(t.alias, t.name) AS 'tag'
-  FROM tags t
+ FROM tags t
  WHERE tag_id IN (
    SELECT tag_id FROM links l JOIN link_tags USING (link_id)
-    WHERE l.title = 'a'
+   WHERE l.title = 'a'
  );
 
 -- links for tag
 SELECT l.* 
-  FROM links l JOIN link_tags USING (link_id)
-       JOIN tags t USING (tag_id)
+ FROM links l JOIN link_tags USING (link_id)
+   JOIN tags t USING (tag_id)
  WHERE t.name = 'x' OR t.alias = 'x';
 
 SELECT l.*
-  FROM links l
+ FROM links l
  WHERE link_id IN (
    SELECT link_id FROM tags t JOIN link_tags USING (tag_id)
-    WHERE t.name = 'x' OR t.alias = 'x'
+   WHERE t.name = 'x' OR t.alias = 'x'
  );
 
 -- tags for group
 SELECT t.*
-  FROM groups g
-       JOIN group_tags USING (group_id)
-       JOIN tags t USING (tag_id)
+ FROM groups g
+   JOIN group_tags USING (group_id)
+   JOIN tags t USING (tag_id)
  WHERE g.name = 'xy';
 
 SELECT t.*
-  FROM tags t 
+ FROM tags t 
  WHERE tag_id IN (
    SELECT tag_id 
-     FROM groups g JOIN group_tags USING (group_id)
-    WHERE g.name = 'xy'
+   FROM groups g JOIN group_tags USING (group_id)
+   WHERE g.name = 'xy'
  );
 
 -- links for group
 SELECT DISTINCT l.*
-  FROM links l
-       JOIN link_tags USING (link_id)
-       JOIN group_tags USING (tag_id)
-       JOIN groups g USING (group_id)
+ FROM links l
+   JOIN link_tags USING (link_id)
+   JOIN group_tags USING (tag_id)
+   JOIN groups g USING (group_id)
  WHERE g.name = 'xy';
 
 SELECT DISTINCT l.*
-  FROM links l JOIN link_tags USING (link_id)
+ FROM links l JOIN link_tags USING (link_id)
  WHERE tag_id IN (
    SELECT tag_id
-     FROM groups g JOIN group_tags USING (group_id)
-    WHERE g.name = 'xy'
+   FROM groups g JOIN group_tags USING (group_id)
+   WHERE g.name = 'xy'
  );
 
 -- groups for link
 SELECT DISTINCT g.*
-  FROM groups g JOIN group_tags USING (group_id)
-       JOIN link_tags USING (tag_id)
-       JOIN links l USING (link_id)
+ FROM groups g JOIN group_tags USING (group_id)
+   JOIN link_tags USING (tag_id)
+   JOIN links l USING (link_id)
  WHERE l.title = 'a';
 
 SELECT DISTINCT g.*
-  FROM groups g JOIN group_tags USING (group_id)
+ FROM groups g JOIN group_tags USING (group_id)
  WHERE tag_id IN (
    SELECT tag_id
-     FROM links l JOIN link_tags USING (link_id)
-    WHERE l.title = 'a'
-);
+   FROM links l JOIN link_tags USING (link_id)
+   WHERE l.title = 'a'
+ );
